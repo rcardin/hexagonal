87157201226a11725fb67fa19cdcfc2d4f84b828
rcardin <riccardo.cardin@gmail.com>
2020-05-16 15:10:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hexagonal Architecture
+
+This toy project shows how to implement a service using Spring Boot, Kotlin, and the Hexagonal Architecture.
---
+++ src/test/kotlin/io/rcardin/hexagonal/HexagonalArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.rcardin.hexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
