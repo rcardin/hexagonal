dc6018c6dfd4ef107bfc5e1eab375578f5c24519
rcardin <riccardo.cardin@gmail.com>
2020-05-17 15:37:54 +0200

    Added logging to portfolio creation port implementation

--- src/main/kotlin/io/rcardin/hexagonal/portfolio/PortfolioPersistenceAdapter.kt
+++ src/main/kotlin/io/rcardin/hexagonal/portfolio/PortfolioPersistenceAdapter.kt
@@ -1,15 +1,20 @@
 package io.rcardin.hexagonal.portfolio
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 class PortfolioPersistenceAdapter(
         private val repository: PortfolioRepository
 ): PortfolioCreationPort {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     override suspend fun createPortfolio(portfolio: Portfolio): Boolean {
         return try {
             repository.save(portfolio)
             true
         } catch (e: Exception) {
-            // TODO Add some logging
+            logger.error("Error during the persistence of the portfolio {}", portfolio.name)
             false
         }
     }
