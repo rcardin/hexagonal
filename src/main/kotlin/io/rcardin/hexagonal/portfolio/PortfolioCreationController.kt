6ef04e6896ed472798a92caea12a6bc95cd82e43
rcardin <riccardo.cardin@gmail.com>
2020-05-18 22:04:53 +0200

    Added the controller that handles the creation of a new portfolio

---
+++ src/main/kotlin/io/rcardin/hexagonal/portfolio/PortfolioCreationController.kt
@@ -0,0 +1,22 @@
+package io.rcardin.hexagonal.portfolio
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+
+@RestController
+class PortfolioCreationController(private val useCase: PortfolioCreationUseCase) {
+
+    @PostMapping("/portfolios")
+    suspend fun createPortfolio(@RequestBody name: String): ResponseEntity<Any> {
+        val command = PortfolioCreationUseCase.PortfolioCreationCommand(name)
+        val created = useCase.createPortfolio(command)
+        return if (created) {
+            ResponseEntity.created(URI.create("/portfolios/$name")).build()
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+}
