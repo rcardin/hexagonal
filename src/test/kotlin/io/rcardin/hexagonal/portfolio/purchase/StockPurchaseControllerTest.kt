92d65ae4cbb7a3393840a5a43435afc50d6e88d9
rcardin <riccardo.cardin@gmail.com>
2020-06-18 22:04:56 +0200

    Added the implementation of the StockPurchaseControllerTest

--- src/test/kotlin/io/rcardin/hexagonal/portfolio/purchase/StockPurchaseControllerTest.kt
+++ src/test/kotlin/io/rcardin/hexagonal/portfolio/purchase/StockPurchaseControllerTest.kt
@@ -1,14 +1,15 @@
 package io.rcardin.hexagonal.portfolio.purchase
 
-import io.rcardin.hexagonal.portfolio.creation.PortfolioCreationController
-import io.rcardin.hexagonal.portfolio.creation.PortfolioCreationUseCase
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
 
-@WebFluxTest(controllers = [ StockPurchaseController::class ])
+@WebFluxTest(controllers = [StockPurchaseController::class])
 internal class StockPurchaseControllerTest {
 
     @MockBean
@@ -18,7 +19,31 @@ internal class StockPurchaseControllerTest {
     lateinit var client: WebTestClient
 
     @Test
-    fun buyStock() {
-        // TODO
+    fun `Buy a stock should return a 200 status if everything is ok`() = runBlocking {
+        val command = StockPurchaseUseCase.StockPurchaseCommand(
+                "portfolio", "AAPL", 1000L)
+        whenever(userCase.buy(command)).thenReturn(true)
+        client.put()
+                .uri("/portfolios/portfolio/stocks/AAPL")
+                .body(BodyInserters.fromValue(1000L))
+                .exchange()
+                .expectStatus()
+                .isOk
+        Unit
     }
+
+    @Test
+    fun `Buy a stock should return a 404 status if the portfolio does not exist`() =
+            runBlocking {
+                val command = StockPurchaseUseCase.StockPurchaseCommand(
+                        "portfolio", "AAPL", 1000L)
+                whenever(userCase.buy(command)).thenReturn(false)
+                client.put()
+                        .uri("/portfolios/portfolio/stocks/AAPL")
+                        .body(BodyInserters.fromValue(1000L))
+                        .exchange()
+                        .expectStatus()
+                        .isNotFound
+                Unit
+            }
 }
