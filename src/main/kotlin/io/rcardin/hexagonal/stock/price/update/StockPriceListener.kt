43a2906a196d3196778d2f3adbfa32f4c21ba094
rcardin <riccardo.cardin@gmail.com>
2020-10-09 22:02:04 +0200

    Added Spring Kafka to the project. Started the development of the Kafka Listener for Stock price update

---
+++ src/main/kotlin/io/rcardin/hexagonal/stock/price/update/StockPriceListener.kt
@@ -0,0 +1,29 @@
+package io.rcardin.hexagonal.stock.price.update
+
+import kotlinx.coroutines.runBlocking
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Component
+
+@Component
+class StockPriceListener(private val useCase: StockPriceUpdateUseCase) {
+    @KafkaListener(
+            id = "priceListener",
+            topics = [ "prices" ],
+            groupId = "prices",
+            properties = [
+                "key.deserializer=org.apache.kafka.common.serialization.StringDeserializer",
+                "value.deserializer=org.apache.kafka.common.serialization.DoubleDeserializer"
+            ]
+    )
+    fun listenToPrices(
+            @Header(name = KafkaHeaders.RECEIVED_MESSAGE_KEY) stock: String,
+            @Payload price: Double
+    ) {
+        runBlocking {
+            useCase.updatePrice(StockPriceUpdateUseCase.StockPriceUpdateCommand(stock, price))
+        }
+    }
+}
--- src/main/kotlin/io/rcardin/hexagonal/stock/price/update/StockPriceUpdateConfiguration.kt
+++ src/main/kotlin/io/rcardin/hexagonal/stock/price/update/StockPriceUpdateConfiguration.kt
@@ -2,7 +2,9 @@ package io.rcardin.hexagonal.stock.price.update
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
 
+@EnableKafka
 @Configuration
 class StockPriceUpdateConfiguration {
     @Bean
