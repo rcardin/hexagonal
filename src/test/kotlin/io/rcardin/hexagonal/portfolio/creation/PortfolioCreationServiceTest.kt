cc05d9f2f0305fcee625c52091d1f980c5c899af
rcardin <riccardo.cardin@gmail.com>
2020-10-25 21:29:40 +0100

    Changed some tests that uses Mockito in a more kotlinish style

--- src/test/kotlin/io/rcardin/hexagonal/portfolio/creation/PortfolioCreationServiceTest.kt
+++ src/test/kotlin/io/rcardin/hexagonal/portfolio/creation/PortfolioCreationServiceTest.kt
@@ -1,30 +1,23 @@
 package io.rcardin.hexagonal.portfolio.creation
 
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import io.rcardin.hexagonal.portfolio.Portfolio
 import io.rcardin.hexagonal.portfolio.creation.PortfolioCreationUseCase.PortfolioCreationCommand
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.junit.jupiter.MockitoExtension
 
-@ExtendWith(MockitoExtension::class)
 internal class PortfolioCreationServiceTest {
-
-    @Mock
-    lateinit var creationPort: PortfolioCreationPort
-
-    @InjectMocks
-    lateinit var service: PortfolioCreationService
 
     @Test
     internal fun `The creation of a portfolio should return the result of the persistence operation`() =
             runBlocking {
                 val portfolio = Portfolio("portfolio")
-                whenever(creationPort.createPortfolio(portfolio)).thenReturn(true)
+                val creationPort: PortfolioCreationPort = mock {
+                    onBlocking { createPortfolio(portfolio) } doReturn true
+                }
+                val service = PortfolioCreationService(creationPort)
                 Assertions.assertTrue(
                     service.createPortfolio(PortfolioCreationCommand("portfolio"))
                 )
--- src/test/kotlin/io/rcardin/hexagonal/stock/price/StockPricePersistenceAdapterTest.kt
+++ src/test/kotlin/io/rcardin/hexagonal/stock/price/StockPricePersistenceAdapterTest.kt
@@ -1,27 +1,18 @@
 package io.rcardin.hexagonal.stock.price
 
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.doThrow
+import com.nhaarman.mockitokotlin2.mock
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.junit.jupiter.MockitoExtension
 
-@ExtendWith(MockitoExtension::class)
 internal class StockPricePersistenceAdapterTest {
-
-    @Mock
-    lateinit var repository: StockPriceRepository
-
-    @InjectMocks
-    lateinit var adapter: StockPricePersistenceAdapter
 
     @Test
     internal fun `The update of a stock price should return true if everything goes fine`() {
         runBlocking {
+            val adapter = StockPricePersistenceAdapter(mock())
             val result = adapter.updatePrice(Price("AAPL", 1234.45))
             assertTrue(result)
         }
@@ -31,7 +22,10 @@ internal class StockPricePersistenceAdapterTest {
     internal fun `The update of a stock price should return false if something goes wrong`() {
         runBlocking {
             val price = Price("AAPL", 1234.45)
-            whenever(repository.save(price.toMongo())).thenThrow(RuntimeException::class.java)
+            val repository: StockPriceRepository = mock {
+                onBlocking { save(price.toMongo()) } doThrow RuntimeException::class
+            }
+            val adapter = StockPricePersistenceAdapter(repository)
             val result = adapter.updatePrice(Price("AAPL", 1234.45))
             assertFalse(result)
         }
